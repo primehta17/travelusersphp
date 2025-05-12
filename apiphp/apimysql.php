5b3545b73612a9c5baa8129d1bfe172f42125dcb
Priyanka Mehta <primehta17@gmail.com>
2025-05-12 23:51:24 +0530

    api result

--- apiphp/apimysql.php
+++ apiphp/apimysql.php
@@ -1,60 +1,57 @@
 <?php 
 
-include('./db');
-header("Content-Type","application/json");
+include('./db.php');
+header("Content-Type: application/json");
 
 $method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents("php://input"), true);
 
-$input = json_decode(file_get_contents("php://input"),true);
-
-switch($method){
+switch ($method) {
     case 'GET':
-        if(isset($_GET['id'])){
-            $id = $_GET['id'];
-            $query = mysqli_query($conn,'select * from apiusers where id= "$id');
-
-           echo json_encode(fetch_assoc($query));
-        }else{
-            $query = mysqli_query($conn,'select * from apiusers');
-            $data=[];
-            while($row= mysqli_fetch_assoc($query)){
+        if (isset($_GET['id'])) {
+            $id = intval($_GET['id']);
+            $query = mysqli_query($conn, "SELECT * FROM apiusers WHERE id = $id");
+            echo json_encode(mysqli_fetch_assoc($query));
+        } else {
+            $query = mysqli_query($conn, "SELECT * FROM apiusers");
+            $data = [];
+            while ($row = mysqli_fetch_assoc($query)) {
                 $data[] = $row;
             }
-         echo   json_encode($data);
+            echo json_encode($data);
         }
         break;
+
     case 'POST':
         $name = $input['name'];
-        $email =$input['email'];
-        $query= mysqli_query($conn,"Insert into apiusers (name,email) values ($name,$email)");
-        json_encode("message:Succefully inserted");
+        $email = $input['email'];
+        $query = mysqli_query($conn, "INSERT INTO apiusers (name, email) VALUES ('$name', '$email')");
+        echo json_encode(["message" => "Successfully inserted"]);
         break;
 
-    case "PUT":
-        if(isset($id)){
-           echo "enter valid id";
-            
-        }else{
-            $id= $_GET['id'];
-            $name = $input['name'];
-            $email =$input['email'];
-            $query= mysqli_query($conn,"update apiusers set name='$name' , email='$email' where id='$id");
-           echo json_encode(["status"=>200,"message"=>"Updateed succesfully"]);
-           
-        }; 
-        break;
-        case "DELETE":
-            $id= $_GET['id'];
-            $query =mysqli_query($conn,"Delete from apiusers where id='$id'");
-            echo json_encode(["message"=>"Delete succedful"]);
+    case 'PUT':
+        if (!isset($_GET['id'])) {
+            echo json_encode(["error" => "ID is required for update"]);
             break;
-      case "DEFAULT":
-        echo "no method matched";
+        }
+        $id = intval($_GET['id']);
+        $name = $input['name'];
+        $email = $input['email'];
+        $query = mysqli_query($conn, "UPDATE apiusers SET name='$name', email='$email' WHERE id=$id");
+        echo json_encode(["status" => 200, "message" => "Updated successfully"]);
+        break;
 
+    case 'DELETE':
+        if (!isset($_GET['id'])) {
+            echo json_encode(["error" => "ID is required for delete"]);
+            break;
+        }
+        $id = intval($_GET['id']);
+        $query = mysqli_query($conn, "DELETE FROM apiusers WHERE id=$id");
+        echo json_encode(["message" => "Deleted successfully"]);
+        break;
 
-
+    default:
+        echo json_encode(["error" => "No method matched"]);
 }
-
-
-
 ?>
